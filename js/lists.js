2165ea658abc4a87224ae0832622899d3b9eaf86
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2020-12-28 16:54:21 +0100

    Fix issues with search page

--- js/lists.js
+++ js/lists.js
@@ -1,11 +1,22 @@
 function showRecipe(id) {
-    __buildUrl(id);
+    __buildUrl(id, 0);
 }
 
 function showIngredient(id) {
-    __buildUrl(id);
+    __buildUrl(id, 1);
 }
 
-function __buildUrl(id) {
-    window.location.href = window.location.href + '/' + id;
+function __buildUrl(id, type) {
+    if (window.location.href.includes('search')) {
+        switch (type) {
+            case 0:
+                window.location.href = window.location.href.replace('search', 'recipe') + '/' + id;
+                break;
+            case 1:
+                window.location.href = window.location.href.replace('search', 'ingredient') + '/' + id;
+                break;
+        }
+    } else {
+        window.location.href = window.location.href + '/' + id;
+    }
 }
