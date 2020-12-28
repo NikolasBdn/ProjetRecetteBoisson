5fc2e44b6791d85ab2d40820f61a1302065b2feb
NikolasBdn <nicolas.baudon21@gmail.com>
2020-12-28 13:43:18 +0100

    Correction suppression recette panier

--- src/controllers/CartController.php
+++ src/controllers/CartController.php
@@ -12,9 +12,9 @@ class CartController
 {
   public static function cart() {
       $arg = array();
-
       if (isset($_COOKIE['cart'])) {
         $cart = json_decode($_COOKIE['cart']);
+        var_dump($cart);
         foreach ($cart as $id) {
             $recette = Recette::where('id', '=', $id)->first();
             array_push($arg, $recette);
@@ -37,21 +37,17 @@ class CartController
           setcookie('cart',  json_encode($cart), time()+3600*24*365, '/',  $_SERVER['HTTP_HOST']);
       }
 
-      printf($_COOKIE['cart']);
-      echo $_SERVER['HTTP_HOST']."/recipe/".$id;
-      // header("Location: http://".$_SERVER['HTTP_HOST']."/recipe/".$id);
-      // exit();
-
       return "";
   }
 
   public static function delete($id) {
       if (isset($_COOKIE['cart']) && in_array($id, json_decode($_COOKIE['cart']))) {
           $cart = json_decode($_COOKIE['cart']);
-          $cart = array_diff($cart, array($id));
+          $index = array_search($id, $cart);
+          printf($index);
+          array_splice($cart, $index, ++$index);
           setcookie('cart',  json_encode($cart), time()+3600*24*365, '/',  $_SERVER['HTTP_HOST']);
     }
-    printf($_COOKIE['cart']);
 
     header("Location: http://".$_SERVER['HTTP_HOST']."/recipe/".$id);
     exit();
