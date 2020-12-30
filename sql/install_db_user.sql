92d1fc253e43d76e7ae7ea277c6b0430c230c1df
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2020-12-30 20:16:35 +0100

    Fix sql

--- sql/install_db_user.sql
+++ sql/install_db_user.sql
@@ -1,12 +1,12 @@
-create table User(
-    username varchar2(256),
-    password varchar2(256),
+create table User (
+    username varchar(64),
+    password varchar(256),
     primary key (username)
 );
 
 create table basket(
-    username varchar2(256),
+    username varchar(64),
     recipe   int,
-    constraint fk_recipe foreign key (recipe) references Recette (recipe),
+    constraint fk_recipe foreign key (recipe) references Recette (id),
     constraint fk_username foreign key (username) references User (username)
 );
